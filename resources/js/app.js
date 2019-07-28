6525c32f6d8163564cff7fbf80c1de4b94d7067c
Risin9 <Risin9@foxmail.com>
2019-07-28 16:20:00 +0800

    引入Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,33 @@
 //remove bootstrap
 //require('./bootstrap');
 
+window._ = require('lodash');
+try {
+    windows.$ = windows.jQuery = require('jquery');
+    require('foundation-sites');
+} catch (e) {
+    
+}
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+
 window.Vue = require('vue');
 
 /**
