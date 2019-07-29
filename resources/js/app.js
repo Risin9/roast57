c85f239945c1405de999761f23618763ce4e38f0
Risin9 <Risin9@foxmail.com>
2019-07-29 19:18:15 +0800

    添加VUE页面及引入Router

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,9 +35,13 @@ if (token) {
 
 import Vue from 'vue';
 
+import router from './routes.js';
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
-new Vue().$mount('#app');
+new Vue({
+    router
+}).$mount('#app');
