63baf46e498817dfb15ac6da9f9ccbb046a42484
Risin9 <Risin9@foxmail.com>
2019-07-31 19:09:57 +0800

    配置Vuex中的action属性

---
+++ resources/js/modules/cafes.js
@@ -0,0 +1,53 @@
+/**
+ * -------------------------------------
+ * Vuex modules/cafes.js
+ * -------------------------------------
+ * The vuex data store for the cafes
+ */
+import CafeAPI from '../api/cage.js';
+
+export const cafes = {
+    state: {
+        /**
+         * status:
+         *  0: waiting load
+         *  1: loading
+         *  2: success
+         *  3：failed
+         */
+        cafes: [],
+        cafesLoadStatus: 0,
+
+        cafe: [],
+        cafeLoadStatus: 0
+    },
+
+    actions: {
+        loadCafes( { commit } ){
+            commit( 'setCafesLoadStatus', 1);
+
+            CafeAPI.getCafes()
+                .then(function (response) {
+                    commit( 'setCafes', response.data);
+                    commit( 'setCafesLoadStatus', 2);
+                })
+                .catch(function () {
+                    commit('setCafes', []);
+                    commit('setCafesLoadStatus', 3);
+                });
+        },
+        loadCafe( { commit } , data ){
+            commit('setCafeLoadStatus', 1);
+            
+            CafeAPI.getCafe( data.id )
+                .then(function ( response ) {
+                    commit( 'setCafe', response.data );
+                    commit( 'setCafeLoadStatus', 2);
+                }).catch(function () {
+                    commit( 'setCafe', {});
+                    commit( 'setCafeLoadStatus', 3);
+                });
+        }
+    }
+
+}
