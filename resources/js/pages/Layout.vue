b9a5069a4faf2d475744ecaa62acdba4a0f6519c
Risin9 <Risin9@foxmail.com>
2019-08-06 18:50:52 +0800

    添加layout

---
+++ resources/js/pages/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <navigation></navigation>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import Navigation from '../components/global/navigation.vue';
+
+    export default {
+        components: {
+            Navigation
+        },
+        created(){
+            this.$store.dispatch( 'loadCafes' );
+            this.$store.dispatch( 'loadUser' );
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
